0093733de1ed35043518530670d5448cb97b9699
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-09-16 14:24:29 -0500

    completed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
+require("dotenv").config()
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const apiKey = process.env.API_KEY
+
+async function getImage(query) {
+	const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+	const response = await fetch(endpoint)
+	const data = await response.json()
+	let rando = Math.floor(Math.random() * (24 - 0))
+	let wantedImg = data.data[rando].embed_url
+	console.log(wantedImg)
+	return wantedImg
+}
+
+getImage("tombstone")
